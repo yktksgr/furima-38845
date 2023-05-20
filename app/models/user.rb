d5ad9456e2bc78a643d332cb2284a329230da26f
yktksgr <ymmt.yshr1006@gmail.com>
2023-05-20 20:40:40 +0900

    rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #has_many :items
-  #has_many :purchases
+  # has_many :items
+  # has_many :purchases
 
   validates :nickname, presence: true
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]/ }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,20 +89,20 @@ RSpec.describe User, type: :model do
       it '半角英字のみでは登録できないこと' do
         @user.password = 'password'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
 
       it '半角数字のみでは登録できないこと' do
         @user.password = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
       end
 
       it '全角文字を含むと登録できないこと' do
         @user.password = 'パスワード123'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password には英字と数字の両方を含めて設定してください")
-      end 
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて設定してください')
+      end
 
       it 'passwordとpassword_confirmationが不一致では登録できない' do
         @user.password = 'password1'
