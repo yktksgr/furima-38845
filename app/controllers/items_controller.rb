7195313ad654c164b0cd5e50c53e94cd54f9dac8
yktksgr <ymmt.yshr1006@gmail.com>
2023-05-21 17:05:01 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,9 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price, :image)
+  end
+
 end
