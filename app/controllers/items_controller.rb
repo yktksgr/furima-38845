3b4ecde8e27a4768912aebeba55036d042366f46
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-03 14:54:59 +0900

    出品した商品の情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,20 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: "商品を出品しました。"
+    else
+      redirect_to new_item_path,flash: { error: @item.errors.full_messages }
+    end
   end
 
   def item_params
