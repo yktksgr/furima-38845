e555a3d0e35d10125b774217896fe2c980e62deb
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-04 18:17:26 +0900

    ルーティング、コントローラーの設定と詳細ページへのリンク設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,10 @@ class ItemsController < ApplicationController
     @items = Item.all.order(created_at: :desc)
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def new
     @item = Item.new
   end
