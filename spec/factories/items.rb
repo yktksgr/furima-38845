14e64edb7711d5af877538d122149924ffb9b957
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-04 11:16:09 +0900

    itemモデルの単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    name               { "名前" }
+    description        { "説明" }
+    category_id        { 1 }
+    status_id          { 1 }
+    shipping_cost_id   { 1 }
+    prefecture_id      { 1 }
+    shipping_date_id   { 1 }
+    price              { 1000 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.build(:item, user: @user)
+  end
+
+  describe '商品情報入力' do
+    context '商品情報入力成功時' do
+      it '全ての値が正しく入力されていれば出品できること' do
+        @item.category_id = 2
+        @item.status_id = 2
+        @item.shipping_cost_id = 2
+        @item.prefecture_id = 2
+        @item.shipping_date_id = 2
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品情報の入力失敗時' do
+      it 'imageが空だと出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'nameが空だと出品できない' do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'descriptionが空だと出品できない' do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it 'categoryが未選択だと出品できない' do
+        @item.category_id = 0
+        @item.valid?
+      end
+      it 'statusが未選択だと出品できない' do
+        @item.status_id = 0
+        @item.valid?
+      end
+      it 'shipping_costが未選択だと出品できない' do
+        @item.shipping_cost_id = 0
+        @item.valid?
+      end
+      it 'prefectureが未選択だと出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+      end
+      it 'shipping_dateが未選択だと出品できない' do
+        @item.shipping_date_id = 0
+        @item.valid?
+      end
+      it 'priceが空だと出品できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが全角数字だと出品できない' do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
 end
