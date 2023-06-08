f0921ee7c129ff637ef7593e7b69454102fb5ceb
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-08 23:53:47 +0900

    ページ遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,10 +25,12 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
   end
 
   def update
     @item = Item.find(params[:id])
+    redirect_to root_path unless current_user == @item.user
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
