ff41e3c3f52365fd0cabdb6ce82ab50bc4adaef8
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-09 23:50:57 +0900

    PurchaseFormのバリデーションの記載

--- app/models/purchase_form.rb
+++ app/models/purchase_form.rb
@@ -1,8 +1,16 @@
 class PurchaseForm
   include ActiveModel::Model
-  attr_accessor :hoge, :fuga
+  attr_accessor :item_id, :user_id, :postcode, :prefecture_id, :city, :address, :building_name, :phone_number
 
-  # バリデーションの処理を書く
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/,message: "is invalid. Include hyphen(-)"}
+  end
 
   def save
     # 各テーブルにデータを保存する処理を書く
