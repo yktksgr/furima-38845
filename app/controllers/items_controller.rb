017e6e124702aeff09aec9a9e5d7b4ef6251cea2
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-09 15:57:37 +0900

    削除ボタンのリンクパスとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show,:edit, :update]
-  before_action :check_user_ownership, only: [:edit, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_item, only: [:show,:edit, :update, :destroy]
+  before_action :check_user_ownership, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -33,6 +33,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
