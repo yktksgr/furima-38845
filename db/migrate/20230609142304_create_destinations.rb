cf5312f0bd88a36543a31dd813f1aac6772fddc5
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-10 00:01:04 +0900

    マイグレーションファイルの記述

--- db/migrate/20230609142304_create_destinations.rb
+++ db/migrate/20230609142304_create_destinations.rb
@@ -1,7 +1,13 @@
 class CreateDestinations < ActiveRecord::Migration[6.0]
   def change
     create_table :destinations do |t|
-
+      t.references :purchases,         null: false, foreign_key: true
+      t.string     :postcode,          null: false
+      t.integer    :prefecture_id,     null: false
+      t.string     :city,              null: false
+      t.string     :address,             null: false
+      t.string     :building_name
+      t.string     :phone_number,      null: false
       t.timestamps
     end
   end
