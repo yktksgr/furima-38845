6448148061c6fa622d8fb12e3ee16a46fa9ced4e
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-11 23:16:29 +0900

    トークン化における実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,8 +1,25 @@
 const pay = () => {
+  const payjp = Payjp(''); // PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#card-number');
+  expiryElement.mount('#card-exp-month');
+  cvcElement.mount('#card-cvc');
+
   const submit = document.getElementById("button");
+
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火")
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
+      } else {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
