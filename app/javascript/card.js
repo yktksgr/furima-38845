7c4085df5891cbb33b8cb090f65b5bc0925acaa2
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-14 00:28:53 +0900

    トークン化の修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -6,18 +6,27 @@ const pay = () => {
   const cvcElement = elements.create('cardCvc');
 
   numberElement.mount('#card-number');
-  expiryElement.mount('#card-exp-month');
+  expiryElement.mount('#card-exp-month'); 
+  expiryElement.mount('#card-exp-year'); 
   cvcElement.mount('#card-cvc');
 
   const submit = document.getElementById("button");
 
   submit.addEventListener("click", (e) => {
     e.preventDefault();
+    const cardNumberValue = document.getElementById('card-number').value; // カード番号の値を取得
+    const expMonthValue = document.getElementById('card-exp-month').value; // 有効期限（月）の値を取得
+    const expYearValue = document.getElementById('card-exp-year').value; // 有効期限（年）の値を取得
+    const cvcValue = document.getElementById('card-cvc').value; // セキュリティコードの値を取得
+
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
+        // エラーハンドリングの処理を記述
       } else {
         const token = response.id;
-        console.log(token)
+        console.log(token);
+        // トークンをサーバーに送信する処理を記述
+        // ここでサーバー側にトークンを送信し、支払い処理を行うことができます
       }
     });
   });
