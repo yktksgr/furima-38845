e09794070302df378ce57682c95d68560c93d604
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-14 23:56:51 +0900

    トークン化における修正（formwith→divクラスに）

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,32 +1,23 @@
 const pay = () => {
-  const payjp = Payjp(''); // PAY.JPテスト公開鍵
+  const payjp = Payjp('pk_test_***********************')// PAY.JPテスト公開鍵
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
 
-  numberElement.mount('#card-number');
-  expiryElement.mount('#card-exp-month'); 
-  expiryElement.mount('#card-exp-year'); 
-  cvcElement.mount('#card-cvc');
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
 
   const submit = document.getElementById("button");
 
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    const cardNumberValue = document.getElementById('card-number').value; // カード番号の値を取得
-    const expMonthValue = document.getElementById('card-exp-month').value; // 有効期限（月）の値を取得
-    const expYearValue = document.getElementById('card-exp-year').value; // 有効期限（年）の値を取得
-    const cvcValue = document.getElementById('card-cvc').value; // セキュリティコードの値を取得
-
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
-        // エラーハンドリングの処理を記述
       } else {
         const token = response.id;
-        console.log(token);
-        // トークンをサーバーに送信する処理を記述
-        // ここでサーバー側にトークンを送信し、支払い処理を行うことができます
+        console.log(token)
       }
     });
   });
