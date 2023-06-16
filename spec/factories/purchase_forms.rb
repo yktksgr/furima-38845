116b164d4fb233b09ffb183d31b511c485e0b8f5
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-17 00:15:23 +0900

    テストコードの修正

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_form do
-    item_id { 1 }
-    user_id { 1 }
+    association :user
+    association :item
     postcode { Faker::Number.number(digits: 3).to_s + '-' + Faker::Number.number(digits: 4).to_s }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     city { Faker::Address.city }
--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe PurchaseForm, type: :model do
   before do
-    @purchase_form = FactoryBot.build(:purchase_form)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @purchase_form = FactoryBot.build(:purchase_form, user_id: @user.id, item_id: @item.id)
+    sleep 0.1
   end
 
   describe '配送先情報の保存' do
@@ -10,48 +13,20 @@ RSpec.describe PurchaseForm, type: :model do
       it 'すべての値が正しく入力されていれば保存できること' do
         expect(@purchase_form).to be_valid
       end
-      it 'user_idが空でなければ保存できる' do
-        @purchase_form.user_id = 1
-        expect(@purchase_form).to be_valid
-      end
-      it 'item_idが空でなければ保存できる' do
-        @purchase_form.item_id = 1
-        expect(@purchase_form).to be_valid
-      end
-      it '郵便番号が「3桁+ハイフン+4桁」の組み合わせであれば保存できる' do
-        @purchase_form.postcode = '123-4567'
-        expect(@purchase_form).to be_valid
-      end
-      it '都道府県が未選択でなければ保存できる' do
-        @purchase_form.prefecture_id = 2
-        expect(@purchase_form).to be_valid
-      end
-      it '市区町村が空でなければ保存できる' do
-        @purchase_form.city = '伊予市'
-        expect(@purchase_form).to be_valid
-      end
-      it '番地が空でなければ保存できる' do
-        @purchase_form.address = '青山区１２３'
-        expect(@purchase_form).to be_valid
-      end
       it '建物名が空でも保存できる' do
-        @purchase_form.building_name = nil
-        expect(@purchase_form).to be_valid
-      end
-      it '電話番号が11桁以内かつハイフンなしであれば保存できる' do
-        @purchase_form.phone_number = 12_345_678_910
+        @purchase_form.building_name = ''
         expect(@purchase_form).to be_valid
       end
     end
 
     context '配送先情報の保存ができないとき' do
       it 'user_idが空だと保存できない' do
-        @purchase_form.user_id = nil
+        @purchase_form.user_id = ''
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("User can't be blank")
       end
       it 'item_idが空だと保存できない' do
-        @purchase_form.item_id = nil
+        @purchase_form.item_id = ''
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include("Item can't be blank")
       end
@@ -88,6 +63,11 @@ RSpec.describe PurchaseForm, type: :model do
       end
       it '電話番号にハイフンがあると保存できないこと' do
         @purchase_form.phone_number = '123-4567-8900'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include('Phone number is invalid. Include hyphen(-)')
+      end
+      it '電話番号が9桁以下だと購入できないこと' do
+        @purchase_form.phone_number = '090123456'
         @purchase_form.valid?
         expect(@purchase_form.errors.full_messages).to include('Phone number is invalid. Include hyphen(-)')
       end
