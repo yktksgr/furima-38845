2d5a58f18a7c7b261cdf0b95a222bb35aadf47e9
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-27 22:38:54 +0900

    ビューの編集

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,9 +5,8 @@ class CommentsController < ApplicationController
       redirect_to item_path(params[:item_id])
     end
   end
-end
 
-private
+  private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
   end
