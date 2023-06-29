4049e840cfe4d470842278e5d5e09daab8b80066
yktksgr <ymmt.yshr1006@gmail.com>
2023-06-29 22:25:35 +0900

    コメント情報を画面に表示

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,15 +1,26 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("CommentChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+if(location.pathname.match(/\/items\/\d/)){
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  consumer.subscriptions.create("CommentChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      const html = `
+        <div class="comment">
+          <p class="user-info">${data.user.nickname}: </p>
+          <p>${data.comment.text}</p>
+        </div>`
+      const comments = document.getElementById("comments")
+      comments.insertAdjacentHTML('beforeend', html)
+      const commentForm = document.getElementById("comment-form")
+      commentForm.reset();
+    }
+  })
+}
